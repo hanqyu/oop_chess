655d855d8ee3097de640dbf9a9678cd17ef30eb6
hanqyu <peakjnut37@gmail.com>
2019-11-25 11:48:18 +0900

    initial commit

---
+++ src/board/Board.java
@@ -0,0 +1,73 @@
+package board;
+
+import pieces.*;
+import util.Core;
+import util.Move;
+
+import java.util.Iterator;
+
+public class Board {
+
+    public static final int DIMENSION = 8;
+
+    private static Square[][] grid = new Square[DIMENSION][DIMENSION];
+
+    private static Board boardInstance = new Board();
+
+    public static Board getInstance() {
+        return boardInstance;
+    }
+
+    private Board() {
+        initialize();
+    }
+
+    public static void initialize() {
+        initializeSquares();
+        initializePieces();
+    }
+
+    public static Square getSquare(char file, int rank) {
+        file = Character.toLowerCase(file);
+        if (file < 'a' || file > 'h' || rank < 1 || rank > 8) {
+            return null;
+        } else {
+            return grid[file - 'a'][rank - 1];
+        }
+    }
+
+    public static void executeMove(Move move) {
+        Square originSquare = getSquare(move.getOriginFile(), move.getOriginRank());
+        Square destinationSquare = getSquare(move.getDestinationFile(), move.getDestinationRank());
+        if (destinationSquare.getCurrentPiece() != null) {
+            destinationSquare.getCurrentPiece().setCapture(true);
+            PieceSet.addCapturedPiece(destinationSquare.getCurrentPiece());
+        }
+        destinationSquare.setCurrentPiece(originSquare.getCurrentPiece());
+        originSquare.setCurrentPiece(null);
+    }
+
+    private static void initializeSquares() {
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                grid[i][j] = new Square();
+            }
+        }
+    }
+
+    private static void initializePieces() {
+        /*
+        TODO-piece
+            Initialize pieces on board
+            Check following code to implement other pieces
+            Highly recommended to use same template!
+         */
+        // rooks
+        Iterator<Piece> whiteRooksIterator = PieceSet.getPieces(Piece.Color.WHITE, Piece.Type.ROOK).iterator();
+        Iterator<Piece> blackRooksIterator = PieceSet.getPieces(Piece.Color.BLACK, Piece.Type.ROOK).iterator();
+        getSquare('a', 1).setCurrentPiece(whiteRooksIterator.next());
+        getSquare('h', 1).setCurrentPiece(whiteRooksIterator.next());
+        getSquare('a', 8).setCurrentPiece(blackRooksIterator.next());
+        getSquare('h', 8).setCurrentPiece(blackRooksIterator.next());
+    }
+}
---
+++ src/pieces/PieceSet.java
@@ -0,0 +1,110 @@
+package pieces;
+
+import java.util.*;
+
+public class PieceSet {
+
+    private static Map<Piece.Color, Map<Piece.Type, List<Piece>>> pieceSet = null;
+    private static Map<Piece.Color, Stack<Piece>> capturedPieceSet;
+
+    private static char whiteKingFile;
+    private static int whiteKingRank;
+    private static char blackKingFile;
+    private static int blackKingRank;
+
+    private static PieceSet pieceSetInstance = new PieceSet();
+
+    public static PieceSet getInstance() {
+        return pieceSetInstance;
+    }
+
+    private PieceSet() {
+        initialize();
+    }
+
+    private static void initialize() {
+        initializePieceSet();
+        initializeCapturedPieceSet();
+        initializeKingsCoordinates();
+    }
+
+    public static List<Piece> getPieces(Piece.Color color) {
+        List<Piece> piecesSameColor = new ArrayList<Piece>();
+        for (Map.Entry<Piece.Type, List<Piece>> piecesEntry : pieceSet.get(color).entrySet()) {
+            for (Piece piece : piecesEntry.getValue()) {
+                piecesSameColor.add(piece);
+            }
+        }
+        return piecesSameColor;
+    }
+
+    public static List<Piece> getPieces(Piece.Color color, Piece.Type type) {
+        return pieceSet.get(color).get(type);
+    }
+
+    public static void addCapturedPiece(Piece piece) {
+        piece.setCapture(true);
+        capturedPieceSet.get(piece.getColor()).push(piece);
+    }
+
+    public static List<Piece> getCapturedPieces(Piece.Color color) {
+        return capturedPieceSet.get(color);
+    }
+
+    public static char getOpponentKingFile(Piece.Color color) {
+        if (color.equals(Piece.Color.WHITE)) {
+            return blackKingFile;
+        } else {
+            return whiteKingFile;
+        }
+    }
+
+    public static int getOpponentKingRank(Piece.Color color) {
+        if (color.equals(Piece.Color.WHITE)) {
+            return blackKingRank;
+        } else {
+            return whiteKingRank;
+        }
+    }
+
+    private static void initializePieceSet() {
+        /*
+        TODO-piece
+            Initialize pieces and put to pieceSet
+            The structure of pieceSet is as following
+                pieceSet - collection by color - collection by category - each pieces
+            The following code should be good start, but you can fix if you want
+         */
+        pieceSet = new LinkedHashMap<Piece.Color, Map<Piece.Type, List<Piece>>>();
+
+        Map<Piece.Type, List<Piece>> whitePieces = new LinkedHashMap<Piece.Type, List<Piece>>();
+        Map<Piece.Type, List<Piece>> blackPieces = new LinkedHashMap<Piece.Type, List<Piece>>();
+
+        List<Piece> whiteRooks = new ArrayList<Piece>();
+        List<Piece> blackRooks = new ArrayList<Piece>();
+        for (int i = 0; i < 2; i++) {
+            whiteRooks.add(new Rook(Piece.Color.WHITE));
+            blackRooks.add(new Rook(Piece.Color.BLACK));
+        }
+        whitePieces.put(Piece.Type.ROOK, whiteRooks);
+        blackPieces.put(Piece.Type.ROOK, blackRooks);
+
+        pieceSet.put(Piece.Color.WHITE, whitePieces);
+        pieceSet.put(Piece.Color.BLACK, blackPieces);
+    }
+
+    private static void initializeCapturedPieceSet() {
+        capturedPieceSet = new LinkedHashMap<Piece.Color, Stack<Piece>>();
+        Stack<Piece> whiteCapturedPieces = new Stack<Piece>();
+        Stack<Piece> blackCapturedPieces = new Stack<Piece>();
+        capturedPieceSet.put(Piece.Color.WHITE, whiteCapturedPieces);
+        capturedPieceSet.put(Piece.Color.BLACK, blackCapturedPieces);
+    }
+
+    private static void initializeKingsCoordinates() {
+        whiteKingFile = blackKingFile = 'e';
+        whiteKingRank = 1;
+        blackKingRank = 8;
+    }
+
+}
